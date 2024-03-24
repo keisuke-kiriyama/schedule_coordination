68ebad89cdcdd04027b61a3a5eb8aa61ad971139
kiriyama-keisuke <kiriyama_keisuke@r.recruit.co.jp>
2024-03-24 21:45:23 +0900

    スケジュール連携まで

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,7 @@ FROM python:3.9.15
 RUN pip install --upgrade pip
 RUN pip install gspread
 RUN pip install oauth2client
+RUN pip install google-api-python-client
+RUN pip install google-auth
+RUN pip install google-auth-httplib2
+RUN pip install google-auth-oauthlib
--- src/lpp_sheets2calendar.py
+++ src/lpp_sheets2calendar.py
@@ -1,20 +1,24 @@
 import re
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+import datetime
 
 def connect_gspread(jsonf,key):
-    # 認証情報の設定
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
+  scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
 
-    # SpreadSheetへの接続
-    gc = gspread.authorize(credentials)
-    worksheet = gc.open_by_key(key).sheet1
-    return worksheet
+  gc = gspread.authorize(credentials)
+  worksheet = gc.open_by_key(key).sheet1
+  return worksheet
   
-# SpreadSheetから取得したデータから予定を抽出する
+def connect_google_calendar(jsonf):
+  credentials = Credentials.from_service_account_file(jsonf)
+  calendar = build('calendar', 'v3', credentials=credentials)
+  return calendar
+
 def get_schedule(data):
-  # 辞書型のスケジュールを格納するリストを定義する
   schedule = []
   year = '2024'
   for row in range(1,len(data)):
@@ -30,26 +34,39 @@ def get_schedule(data):
     date_pattern = '^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])'
 
     date = year + '/' + re.match(date_pattern, data[row][0]).group()
-    start = data[row][1]
-    end = data[row][2]
-    title = 'LPP: ' + data[row][3]
-    place = data[row][4]
-    station = data[row][5]
-    if not date or not start or not end or not title or not place or not station:
+    start_time = data[row][1]
+    end_time = data[row][2]
+    summary = 'LPP: ' + data[row][3]
+    location = data[row][4]
+    station = '最寄駅: ' + data[row][5]
+    if not date or not start_time or not end_time or not summary or not location or not station:
       continue
 
+    start = datetime.datetime.strptime(date + ' ' + start_time, '%Y/%m/%d %H:%M')
+    end = datetime.datetime.strptime(date + ' ' + end_time, '%Y/%m/%d %H:%M')
     
     # 辞書を構築してscheduleに格納
     schedule.append({
-      'date': date,
-      'start': start,
-      'end': end,
-      'title': title,
-      'place': place,
-      'station': station
+      'summary': summary,
+      'location': location,
+      'description': station,
+      'start': {
+        'dateTime': start.strftime('%Y-%m-%dT%H:%M:%S'),
+        'timeZone': 'Asia/Tokyo',
+      },
+      'end': {
+        'dateTime': end.strftime('%Y-%m-%dT%H:%M:%S'),
+        'timeZone': 'Asia/Tokyo',
+      },
       })
     
- 
+# Google Calendarに予定を登録する
+def register_event(calendar, schedule):
+  for event in schedule:
+    calendarId = 'c3a446e445fd0b159f9ba67960388269ed3815e731cd256c4cc842ba079dec39@group.calendar.google.com'
+    event = calendar.events().insert(calendarId=calendarId, body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
 def main():
   jsonf = '/root/credentials/schedule-coordination-ff9219425143.json'
   spread_sheet_key = '1BHU5xZGcGugcypEGQH9wKF0IiFyJEbfvoSOhfMtaHwg'
@@ -57,7 +74,9 @@ def main():
   worksheet = connect_gspread(jsonf, spread_sheet_key)
   data = worksheet.get_all_values()
   schedule = get_schedule(data)
-  print(schedule)
+
+  calendar = connect_google_calendar(jsonf)
+  register_event(calendar, schedule)
 
 if __name__ == '__main__':
   main()
